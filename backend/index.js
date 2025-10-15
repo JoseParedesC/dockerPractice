621d1b6802b88a5b34aa576c18ec14ed03e3d86c
Jose Dionisio Paredes Caballero <jdparedesc@siesa.com>
2025-10-15 10:52:41 -0500

    cambio de puertos backend

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,6 @@ app.get("/api/prueba", async (req, res) => {
   console.log("Conexión exitosa a Backend");
 });
 
-app.listen(5000, "0.0.0.0", () => {
+app.listen(8080, "0.0.0.0", () => {
   console.log("🚀 Backend corriendo en http://localhost:8080 🤔" );
 });
