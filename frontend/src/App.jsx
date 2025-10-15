90c706fa8b8544b7b8630b79cd76e16b1e56f77a
Jose Dionisio Paredes Caballero <jdparedesc@siesa.com>
2025-10-15 14:03:11 -0500

    changes frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,21 @@ import { useEffect, useState } from "react";
 function App() {
   const API_URL = import.meta.env.VITE_APP_API_URL
   const [prueba, setDataP] = useState(null);
+  const [prueba2, setDataP2] = useState(null);
   const [data, setData] = useState(null);
 
   useEffect(() => {
+    fetch(`https://backend-service-129342308367.us-central1.run.app/api/prueba`) // Nginx redirige al backend cuando esta configurado el proxy en el nginx.conf /api
+      .then(res => res.json())
+      .then(setDataP2)
+      .catch(console.error);
+      
     fetch(`${API_URL}/api/prueba`) // Nginx redirige al backend cuando esta configurado el proxy en el nginx.conf /api
       .then(res => res.json())
       .then(setDataP)
       .catch(console.error);
 
-      fetch(`${API_URL}/api/products`) // Nginx redirige al backend
+    fetch(`${API_URL}/api/products`) // Nginx redirige al backend
       .then(res => res.json())
       .then(setData)
       .catch(console.error);
@@ -21,7 +27,10 @@ function App() {
     <div style={{ padding: "2rem" }}>
       <h1>🚀 Frontend React con Nginx - JdParedesC</h1>
       <div>HOLA </div>
-      <pre>{JSON.stringify(prueba, `No hay conexion al backend\n${API_URL}`, 2)}</pre>
+      <pre>{JSON.stringify(prueba, `No hay conexion al backend`, 2)}</pre>
+      <hr />
+      <pre>{JSON.stringify(prueba2)}</pre>
+      <div><p>${API_URL}</p></div>
       <hr></hr>
       <pre>{JSON.stringify(data, "No hay conexion a la base de datos", 2)}</pre>
     </div>
