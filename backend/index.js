69496d87f345c2132efb90ad39b22059c7ef96fd
Jose Dionisio Paredes Caballero <jdparedesc@siesa.com>
2025-10-15 15:57:04 -0500

    change CORS index.js

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,18 @@ const port = process.env.PORT || 8080
 const app = express();
 
 // Configurar CORS para permitir el dominio del frontend
-// const allowedOrigins = [
-//   "http://localhost:3000", // para desarrollo local
-//   "https://frontend-service-129342308367.us-central1.run.app/" // URL real de tu frontend en Cloud Run
-// ];
+const allowedOrigins = [
+  "http://localhost:3000", // para desarrollo local
+  "https://frontend-service-129342308367.us-central1.run.app/" // URL real de tu frontend en Cloud Run
+];
 
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 
 app.use(express.json());
